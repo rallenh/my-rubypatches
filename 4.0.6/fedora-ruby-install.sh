88d8716c66e2f0d1a9b87bf5c66f5cb1e2b15ade
allenatdecisiv <allen@decisiv.com>
2026-07-16 16:21:09 -0400

    Ruby 4.0.6
    - resync w/Fedora
    - skip the win32 gems
    - updated or added: rustc_flags/CFLAGS/CXXFLAGS/LDFLAGS

---
+++ 4.0.6/fedora-ruby-install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+__RBINSTALL_DIR__=/opt/rubies
+__RUBY_VER__=4.0.6
+
+sudo \
+ruby-install \
+  --no-install-deps \
+  --patch ruby-${__RUBY_VER__}-fedora-patches.patch \
+  --cleanup \
+  --install-dir ${__RBINSTALL_DIR__}/ruby-${__RUBY_VER__} \
+  ruby ${__RUBY_VER__} -- \
+  --build=x86_64-pc-linux-gnu \
+  --host=x86_64-pc-linux-gnu \
+  --exec-prefix=${__RBINSTALL_DIR__}/ruby-${__RUBY_VER__} \
+  --libdir=${__RBINSTALL_DIR__}/ruby-${__RUBY_VER__}/lib \
+  --localstatedir=${__RBINSTALL_DIR__}/ruby-${__RUBY_VER__}/var \
+  --datarootdir=${__RBINSTALL_DIR__}/ruby-${__RUBY_VER__}/share \
+  --with-rubylibprefix=${__RBINSTALL_DIR__}/ruby-${__RUBY_VER__}/lib/ruby \
+  --with-archlibdir=${__RBINSTALL_DIR__}/ruby-${__RUBY_VER__}/lib \
+  '--with-rubyarchprefix=$(rubylibprefix)/$(arch)' \
+  '--with-sitedir=$(rubylibprefix)/site_ruby' \
+  '--with-sitearchdir=$(sitelibdir)/$(sitearch)' \
+  '--with-vendordir=$(rubylibprefix)/vendor_ruby' \
+  '--with-vendorarchdir=$(vendorlibdir)/$(sitearch)' \
+  --with-rubyhdrdir=${__RBINSTALL_DIR__}/ruby-${__RUBY_VER__}/include/ruby \
+  --with-rubyarchhdrdir=${__RBINSTALL_DIR__}/ruby-${__RUBY_VER__}/include/ruby \
+  '--with-sitearchhdrdir=$(sitehdrdir)/$(arch)' \
+  '--with-vendorarchhdrdir=$(vendorhdrdir)/$(arch)' \
+  '--with-sitehdrdir=$(rubyhdrdir)/site_ruby' \
+  '--with-vendorhdrdir=$(rubyhdrdir)/vendor_ruby' \
+  --with-rubygemsdir=${__RBINSTALL_DIR__}/ruby-${__RUBY_VER__}/lib \
+  --with-ruby-pc=ruby.pc \
+  --with-compress-debug-sections=no \
+  --with-out-ext='+,win32*' \
+  --disable-rpath \
+  --disable-shared \
+  --enable-static \
+  --disable-multiarch \
+  --enable-yjit \
+  --enable-rjit \
+  --enable-debug-env \
+  rustc_flags="-Copt-level=3 -Cdebuginfo=2 -Ccodegen-units=1 -Cstrip=none -Cforce-frame-pointers=yes --cap-lints=warn" \
+  CFLAGS="-O2 -flto=auto -ffat-lto-objects -fexceptions -g -grecord-gcc-switches -pipe -Wall -Werror=format-security -Wp,-U_FORTIFY_SOURCE,-D_FORTIFY_SOURCE=3 -Wp,-D_GLIBCXX_ASSERTIONS -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -fstack-protector-strong -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1  -m64 -march=x86-64 -mtune=generic -fasynchronous-unwind-tables -fstack-clash-protection -fcf-protection -mtls-dialect=gnu2 -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer" \
+  CXXFLAGS="-O2 -flto=auto -ffat-lto-objects -fexceptions -g -grecord-gcc-switches -pipe -Wall -Werror=format-security -Wp,-U_FORTIFY_SOURCE,-D_FORTIFY_SOURCE=3 -Wp,-D_GLIBCXX_ASSERTIONS -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -fstack-protector-strong -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1  -m64 -march=x86-64 -mtune=generic -fasynchronous-unwind-tables -fstack-clash-protection -fcf-protection -mtls-dialect=gnu2 -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer" \
+  LDFLAGS="-Wl,-z,relro -Wl,--as-needed  -Wl,-z,pack-relative-relocs -Wl,-z,now -specs=/usr/lib/rpm/redhat/redhat-hardened-ld -specs=/usr/lib/rpm/redhat/redhat-hardened-ld-errors -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1  -Wl,--build-id=sha1"
